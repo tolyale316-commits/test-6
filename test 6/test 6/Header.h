4e01bacf01192880af9cdec44e3407919eaa5907
Студент Студентов <is@athene.tech>
2025-10-17 11:13:11 +0300

    коммит 1

---
+++ test 6/test 6/Header.h
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <windows.h>
+
+// Функция для установки кодировки
+void setEncoding() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+}
+
+// ===== ЗАДАЧА 5.1.1: Високосные годы =====
+int isLeapYear(int year) {
+	if (year % 400 == 0)
+		return 1;
+	if (year % 100 == 0)
+		return 0;
+	if (year % 4 == 0)
+		return 1;
+	return 0;
+}
+
+void task5_1_1() {
+	printf("=== ЗАДАЧА 5.1.1: Високосные годы ===\n");
+	int a, b;
+	printf("Введите начало и конец интервала (два целых числа): ");
+	if (scanf_s("%d %d", &a, &b) != 2) {
+		printf("Неверный ввод\n");
+		return;
+	}
+	printf("Високосные годы в интервале [%d, %d]:\n", a, b);
+
+	int found = 0;
+	for (int num = a; num <= b; num++) {
+		if (isLeapYear(num)) {
+			printf("%d\t", num);
+			found = 1;
+		}
+	}
+	if (!found) {
+		printf("Високосные годы не найдены");
+	}
+	printf("\n");
+}
